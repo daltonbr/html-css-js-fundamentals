a643f604695cdf139d6ea903fbde0ff3981320b4
Dalton Lima <daltonvarussa@gmail.com>
2025-05-20 09:00:44 +0100

    sum function

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const inventory = {
     strawberry: { price: 18, quantity: 0 },
 };
 
+function sum() {
+    let total = 0;
+    for (const key in inventory) {
+        total += inventory[key].price * inventory[key].quantity;
+    }
+
+    // Another way to do it
+    // for (const item of Object.values(inventory)) {
+    //     total += item.price * item.quantity;
+    // }
+
+    // Another way to do it
+    // const keys = Object.keys(inventory);
+    // keys.forEach(key => {
+    //     total += inventory[key].price * inventory[key].quantity
+    // });
+
+    return total;
+}
